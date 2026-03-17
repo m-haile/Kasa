20e7ccafb4f5b42d25aec34c7c827d2329e64481
m-haile <m.hlufafera@gmail.com>
2026-03-17 09:22:19 +0100

    Add ImagePage component and integrate Collapse sections in APropos page

---
+++ src/composants/Image-page/Image-page.jsx
@@ -0,0 +1,6 @@
+import 
+const ImagePage = () => {
+     <img src={montagne} alt="montagne-"/>
+};
+
+export default ImagePage;
--- src/pages/APropos/APropos.jsx
+++ src/pages/APropos/APropos.jsx
@@ -1,7 +1,33 @@
-import "./APropos.scss"
+import Collapse from "../../composants/Collapse/Collapse";
+import "./APropos.scss";
+import ImagePage from "../../composants/Image-page/Image-page.jsx";
 
-export default function APropos(){
-    return (<div className="apropos"></div>)
+export default function APropos() {
+  return (
+    <div className="apropos">
+      <Collapse titre="Fiabilité">
+        Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+        photos sont conformes aux logements, et toutes les informations sont
+        régulièrement vérifiées par nos équipes.
+      </Collapse>
+      <Collapse titre="Respect">
+        La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+        comportement discriminatoire ou de perturbation du voisinage entraînera
+        une exclusion de notre plateforme.
+      </Collapse>
+      <Collapse titre="Service">
+        La qualité du service est au cœur de notre engagement chez Kasa. Nous
+        veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos
+        locataires, soit empreinte de respect et de bienveillance.
+      </Collapse>
+      <Collapse titre="Sécurité">
+        La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour
+        les voyageurs, chaque logement correspond aux critères de sécurité
+        établis par nos services. En laissant une note aussi bien à l'hôte qu'au
+        locataire, cela permet à nos équipes de vérifier que les standards sont
+        bien respectés. Nous organisons également des ateliers sur la sécurité
+        domestique pour nos hôtes.
+      </Collapse>
+    </div>
+  );
 }
-
-exprot default 
