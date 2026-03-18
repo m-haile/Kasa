8ee5ebc3dd8ecee3da2a03abf980c0d076d6562a
m-haile <m.hlufafera@gmail.com>
2026-03-18 14:08:12 +0100

    Refactor App component structure; replace CSS with SCSS, add ImagePage, and update APropos page layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,20 @@ import Footer from "./composants/Footer/Footer.jsx";
 import Thumb from "./composants/Thumb/Thumb.jsx";
 import Collapse from "./composants/Collapse/Collapse.jsx";
 
-import "./App.css";
+import "./App.scss";
 import ImagePage from "./composants/ImagePage/ImagePage.jsx";
 
 import Montagne from "./assets/montagne.png";
+import APropos from "./pages/APropos/APropos.jsx";
 
 function App() {
   return (
     <>
-      {/* <Header />
-      <Accueil />
-      <Collapse titre="Titre">Blabla</Collapse>
-      <Collapse titre="Équipements">
-        Climatisation <br />
-        Wi-Fi <br />
-        Cuisine <br />
-        Espace de travail
-        <br />
-        Fer à repasser
-        <br />
-        Sèche-cheveux
-        <br />
-        Cintres
-        <br />
-      </Collapse>
-      <Thumb
-        titreImage="il était un petit navire il était un petit navire"
-        imageUrl="https://www.popsci.com/wp-content/uploads/2023/05/15/ButterflyFamilyTree.png?quality=85"
-      />
-      <Footer /> */}
-      <ImagePage image={Montagne} titre="L'image de montagne" />
+      <div className="container">
+        <Header />
+        <APropos />
+      </div>
+      <Footer />
     </>
   );
 }
--- src/pages/APropos/APropos.jsx
+++ src/pages/APropos/APropos.jsx
@@ -1,33 +1,37 @@
 import Collapse from "../../composants/Collapse/Collapse";
 import "./APropos.scss";
 import ImagePage from "../../composants/ImagePage/ImagePage.jsx";
+import montagne from "../../assets/montagne.png";
 
 export default function APropos() {
   return (
     <div className="apropos">
-      <Collapse titre="Fiabilité">
-        Les annonces postées sur Kasa garantissent une fiabilité totale. Les
-        photos sont conformes aux logements, et toutes les informations sont
-        régulièrement vérifiées par nos équipes.
-      </Collapse>
-      <Collapse titre="Respect">
-        La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-        comportement discriminatoire ou de perturbation du voisinage entraînera
-        une exclusion de notre plateforme.
-      </Collapse>
-      <Collapse titre="Service">
-        La qualité du service est au cœur de notre engagement chez Kasa. Nous
-        veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos
-        locataires, soit empreinte de respect et de bienveillance.
-      </Collapse>
-      <Collapse titre="Sécurité">
-        La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour
-        les voyageurs, chaque logement correspond aux critères de sécurité
-        établis par nos services. En laissant une note aussi bien à l'hôte qu'au
-        locataire, cela permet à nos équipes de vérifier que les standards sont
-        bien respectés. Nous organisons également des ateliers sur la sécurité
-        domestique pour nos hôtes.
-      </Collapse>
+      <ImagePage image={montagne} alt="Montagne" />
+      <div className="collapse-liste">
+        <Collapse titre="Fiabilité">
+          Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+          photos sont conformes aux logements, et toutes les informations sont
+          régulièrement vérifiées par nos équipes.
+        </Collapse>
+        <Collapse titre="Respect">
+          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
+        </Collapse>
+        <Collapse titre="Service">
+          La qualité du service est au cœur de notre engagement chez Kasa. Nous
+          veillons à ce que chaque interaction, que ce soit avec nos hôtes ou
+          nos locataires, soit empreinte de respect et de bienveillance.
+        </Collapse>
+        <Collapse titre="Sécurité">
+          La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+          pour les voyageurs, chaque logement correspond aux critères de
+          sécurité établis par nos services. En laissant une note aussi bien à
+          l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les
+          standards sont bien respectés. Nous organisons également des ateliers
+          sur la sécurité domestique pour nos hôtes.
+        </Collapse>
+      </div>
     </div>
   );
 }
